66a38583bbe4f9c73747d50d73cb37181a2a3c80
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-21 18:09:16 +0200

    Changer la couleur des boutons au clic (terminé)

--- Cours_android/app/src/main/java/com/example/cours_android/MainActivity.java
+++ Cours_android/app/src/main/java/com/example/cours_android/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cours_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+  public void over(View view) {
+    // Change color of button 1
+    Button thisButton1 = (Button) findViewById(R.id.bouton1);
+    thisButton1.setBackgroundColor(Color.RED);
+
+    // Change color of button 2
+    Button thisButton2 = (Button) findViewById(R.id.bouton2);
+    thisButton2.setBackgroundColor(Color.BLUE);
+
+    // Change color of button 3
+    Button thisButton3 = (Button) findViewById(R.id.bouton3);
+    thisButton3.setBackgroundColor(Color.YELLOW);
+  }
 }
+
