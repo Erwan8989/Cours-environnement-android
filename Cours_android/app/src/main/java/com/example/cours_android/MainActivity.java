f288346fe4b0288c7057199988cc3f926c808174
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-23 10:34:45 +0200

    Image Text

--- Cours_android/app/src/main/java/com/example/cours_android/MainActivity.java
+++ Cours_android/app/src/main/java/com/example/cours_android/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cours_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    @SuppressLint("MissingSuperCall")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+
+        super.onCreate(savedInstanceState);
+        Button buttonClick1;
+        ImageView firstImage;
+        boolean flag;
+        int images[] = {R.drawable.dragon, R.drawable.nike, R.drawable.loup};
+        final int[] i = {0};
+
+        super.onCreate(savedInstanceState);
 
         Log.d(TAG, "onCreate: started"); // Log pour vérifier si le MainActivity a bien pu démarrer
         ImageView FirstImage = (ImageView) findViewById(R.id.FirstImage);  // Création de l'imageVue Object : fait le liens avec ActivityMain.xml
@@ -33,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
         //  Récupération de l'image puis définition dans la vue
         int imageResource = getResources().getIdentifier("@drawable/cat", null, this.getPackageName());
         FirstImage.setImageResource(imageResource);
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstImage = (ImageView) findViewById(R.id.FirstImage);
+        buttonClick1 = (Button) findViewById(R.id.mybutton1);
+
+        flag = true;
+
+        buttonClick1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstImage.setImageResource(images[i[0]]);
+                i[0]++;
+                if (i[0] == 3)
+                    i[0] = 0;
+            }
+        });
     }
 
 
@@ -51,5 +78,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
+
     }
+
 
