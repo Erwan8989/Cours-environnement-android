ec6703abb15ae1f2ab3f4438347e2fd88d85af65
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-23 11:06:51 +0200

    Update NewActivity.java

--- Cours_android/app/src/main/java/com/example/cours_android/NewActivity.java
+++ Cours_android/app/src/main/java/com/example/cours_android/NewActivity.java
@@ -1,4 +1,13 @@
 package com.example.cours_android;
 
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
 public class NewActivity {
 }
