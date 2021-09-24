e19af36850fd50deffc4a0b8ed46ee2aed0db68e
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-24 17:44:34 +0200

    save & load (en cour)

--- Cours_android/app/src/main/java/com/example/cours_android/MainActivity.java
+++ Cours_android/app/src/main/java/com/example/cours_android/MainActivity.java
@@ -15,8 +15,9 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     Button b1, b2, b3;
     ImageView iv;
-    int[] images ={R.drawable.loup,R.drawable.nike,};
+    int[] images ={R.drawable.loup,R.drawable.nike,R.drawable.dragon,};
     int i=0;
+    MediaPlayer mp;
 
 
     @Override
@@ -31,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Son ambiant qui se lance au démmarage de l'application
 
-        music();
+        mp = MediaPlayer.create(this,R.raw.sh);
+        mp.start();
 
         // ***************** Changement de couleur du bouton 1, 2 et 3 et avec changement de l'image au clic *****************
         b1.setOnClickListener(new View.OnClickListener() {
@@ -58,9 +60,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 //Rejouer au début le son ambiant
-                music();
-
-
+                mp.start();
+                mp = MediaPlayer.create(MainActivity.this, R.raw.sh);
+                mp.start();
 
             }
         });
@@ -70,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                mp.stop();
 
                 // Appel à la fonction openActivity pour changer de page (activity)
                 openActivity2();
@@ -98,74 +102,3 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-/*
-
-public class MainActivity extends AppCompatActivity {
-
-    // Création d'un tag pour le login
-    private static final String TAG;
-
-    static {
-        TAG = "MainActivity";
-    }
-
-
-    @SuppressLint("MissingSuperCall")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-
-        super.onCreate(savedInstanceState);
-        Button buttonClick1;
-        ImageView firstImage;
-        boolean flag;
-        int images[] = {R.drawable.dragon, R.drawable.nike, R.drawable.loup};
-        final int[] i = {0};
-
-        super.onCreate(savedInstanceState);
-
-        Log.d(TAG, "onCreate: started"); // Log pour vérifier si le MainActivity a bien pu démarrer
-        ImageView FirstImage = (ImageView) findViewById(R.id.FirstImage);  // Création de l'imageVue Object : fait le liens avec ActivityMain.xml
-
-        //  Récupération de l'image puis définition dans la vue
-        int imageResource = getResources().getIdentifier("@drawable/cat", null, this.getPackageName());
-        FirstImage.setImageResource(imageResource);
-
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        firstImage = (ImageView) findViewById(R.id.FirstImage);
-        buttonClick1 = (Button) findViewById(R.id.mybutton1);
-
-        flag = true;
-
-        buttonClick1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firstImage.setImageResource(images[i[0]]);
-                i[0]++;
-                if (i[0] == 3)
-                    i[0] = 0;
-            }
-        });
-    }
-
-
-    public void buttonClick1(View view) {
-
-        // Change color of button 1
-        Button thisButton1 = (Button) findViewById(R.id.mybutton1);
-        thisButton1.setBackgroundColor(Color.GREEN);
-
-        //Change color of button 2
-        Button thisButton2 = (Button) view;
-        thisButton2.setBackgroundColor(Color.BLUE);
-
-        Button thisButton3 = (Button) view;
-        thisButton2.setBackgroundColor(Color.RED);
-    }
-
-    }
-
-*/
--- Cours_android/app/src/main/java/com/example/cours_android/NewActivity.java
+++ Cours_android/app/src/main/java/com/example/cours_android/NewActivity.java
@@ -6,11 +6,25 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 
 public class NewActivity extends AppCompatActivity {
+
+    // Définition du nom de fichier du texte
+
+    private static final String FILE_NAME ="example.txt";
+    EditText TextEdit;
 
     Button b4, b5, b6, b7;
 
@@ -19,10 +33,75 @@ public class NewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
 
+        TextEdit = findViewById(R.id.TextEdit); }
+
+        // Définition des méthodes Saves & Load
+
+    public void save(View v) {
+        String text = TextEdit.getText().toString();
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+
+            TextEdit.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void load(View v) {
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+
+            TextEdit.setText(sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    ((FileInputStream) fis).close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+
+
 
         // ***************** Revenir à l'activity main *****************
-        b7 = (Button) findViewById(R.id.bouton7);
 
+        b7 = (Button) findViewById(R.id.bouton7);
         b7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
